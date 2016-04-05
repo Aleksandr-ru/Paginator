9e910c0bde4caeef2165456602feef8753ba2f3f
Aleksandr-ru <rebel@pisem.net>
2016-04-05 12:06:22 +0300

    parser

--- Paginator.php
+++ Paginator.php
@@ -9,15 +9,15 @@ class Paginator
 	const DEFAULT_RANGE = 3;
 	
 	protected $formats = array(
-		'json' => FALSE,
+		'json' => '',
 		'bootstrap' => array(
 			'before'        => "<ul class='pagination'>\n",
-			'prev'          => "\t<li><a href='#' aria-label='Previous'><span aria-hidden='true'>&laquo;</span></a></li>\n",
+			'prev'          => "\t<li><a href='{HREF}' aria-label='Previous'><span aria-hidden='true'>&laquo;</span></a></li>\n",
 			'prev-disabled' => "\t<li class='disabled'><span><span aria-hidden='true'>&laquo;</span></span></li>\n",
-			'page'          => "\t<li><a href='#'>1</a></li>\n",
-			'page-active'   => "\t<li class='active'><span>1 <span class='sr-only'>(current)</span></span></li>\n",
+			'page'          => "\t<li><a href='{HREF}'>{PAGE}</a></li>\n",
+			'page-active'   => "\t<li class='active'><span>{PAGE} <span class='sr-only'>(current)</span></span></li>\n",
 			'space'         => "\t<li class='disabled'><span><span aria-hidden='true'>&hellip;</span></span></li>\n",
-			'next'          => "\t<li><a href='#' aria-label='Next'><span aria-hidden='true'>&raquo;</span></a></li>\n",
+			'next'          => "\t<li><a href='{HREF}' aria-label='Next'><span aria-hidden='true'>&raquo;</span></a></li>\n",
 			'next-disabled' => "\t<li class='disabled'><span><span aria-hidden='true'>&raquo;</span></span></li>\n",
 			'after'         => "</ul>\n"
 		)
@@ -60,7 +60,7 @@ class Paginator
 		if($current_page > 1) $this->pages[] = array(
 			'type' => 'prev',
 			'page' => $current_page - 1,
-			'value' => 0				
+			'value' => $min + $limit * ($current_page - 1)
 		);
 		else $this->pages[] = array(
 			'type' => 'prev-disabled'
@@ -74,12 +74,12 @@ class Paginator
 			if($p == $current_page) $this->pages[] = array(
 				'type' => 'page-active',
 				'page' => $current_page,
-				'value' => 0				
+				'value' => $min + $limit * $current_page
 			);	
 			else $this->pages[] = array(
 				'type' => 'page',
 				'page' => $p,
-				'value' => 0				
+				'value' => $min + $limit * $p				
 			);	
 		}
 		// пробел
@@ -90,7 +90,7 @@ class Paginator
 		if($current_page < $total_pages) $this->pages[] = array(
 			'type' => 'next',
 			'page' => $current_page + 1,
-			'value' => 0				
+			'value' => $min + $limit * ($current_page + 1)
 		);
 		else $this->pages[] = array(
 			'type' => 'next-disabled'
@@ -101,15 +101,26 @@ class Paginator
 	{
 		$this->formats[$name] = $format;
 	}
-	
-	function getOutput($href)
+			
+	function getOutput($href, $format)
 	{
-		
+		$ret = '';
+		if(is_array($this->formats[$format])) foreach($this->pages as $p) {
+			$search = array('{PAGE}', '{VALUE}');
+			$replace = array($p['page'], $p['value']);
+			$p['href'] = str_replace($search, $replace, $href);
+			
+			$search = array('{PAGE}', '{HREF}');
+			$replace = array($p['page'], $p['href']);
+			if(isset($this->formats[$format][$p['type']])) $ret .= str_replace($search, $replace, $this->formats[$format][$p['type']]);
+		}
+		else $ret = json_encode($this->pages);
+		return $ret;
 	}
 	
-	function showOutput($href = "?page=%p&value=%v")
+	function showOutput($href = "?page={PAGE}&value={VALUE}", $format = 'json')
 	{
-		return $this->getOutput($href);
+		return $this->getOutput($href, $format);
 	}
 }
 ?>
